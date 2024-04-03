4d510c7e93a9a8f43ad8a08052fe29f8e836458d
arianemadureira <arianemadureira@live.com>
2024-04-02 23:55:37 -0300

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
+// jest.config.js
+
 module.exports = {
-    // Configuração personalizada aqui...
-  };
-  
+  // Diretório onde seus arquivos de teste estão localizados
+  testPathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  // Reporter a ser utilizado para gerar os relatórios
+  reporters: [
+    "default", 
+    ["jest-junit", { outputDirectory: "./test", outputName: "junit.xml" }]
+  ],
+  // Outras opções de configuração do Jest
+};
